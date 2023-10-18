663b77b52f40480eed0da67ba0cdc147ea24485e
NeelClaudel <neel.claudel@gmail.com>
2023-10-18 18:21:36 +0200

    modified:   package.json 	modified:   src/app/github/page.js

--- src/app/github/page.js
+++ src/app/github/page.js
@@ -1,23 +1,21 @@
-'use client'
+"use client";
 
-import useSWR from 'swr'
+import useSWR from "swr";
 
-const fetcher = (url) => fetch(url).then((res)=>res.json());
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function GithubProfile() {
-    const myGithubRepoProfile = "https://api.github.com/repos/codingforentrepreneurs/jref.io"
-    const {data, error, isLoading} = useSWR(myGithubRepoProfile, fetcher)
-    if (error) return "An error happened"
-    if (isLoading) return "Loading..."
-    return (
-        <div>
-            <h1>{data.name}</h1>
-            <p>{data.description}</p>
-            <strong>👁 {data.subscribers_count}</strong>{" "}
-            <strong>✨ {data.stargazers_count}</strong>{" "}
-            <strong>🍴 {data.forks_count}</strong>
-        </div>
-    )
-
-
+  const myGithubRepoProfile = "https://github.com/NeelClaudel/url-shorten.git";
+  const { data, error, isLoading } = useSWR(myGithubRepoProfile, fetcher);
+  if (error) return "An error happened";
+  if (isLoading) return "Loading...";
+  return (
+    <div>
+      <h1>{data.name}</h1>
+      <p>{data.description}</p>
+      <strong>👁 {data.subscribers_count}</strong>{" "}
+      <strong>✨ {data.stargazers_count}</strong>{" "}
+      <strong>🍴 {data.forks_count}</strong>
+    </div>
+  );
 }
