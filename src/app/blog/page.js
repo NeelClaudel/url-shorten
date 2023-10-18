fb955d154bf0367df0f9f78a089be711bf8fc562
NeelClaudel <neel.claudel@gmail.com>
2023-10-18 18:50:36 +0200

    modified:   src/app/blog/page.js

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,41 +1,42 @@
-import getDomain from "@/app/lib/getDomain"
+import getDomain from "@/app/lib/getDomain";
 
-import BlogCard from './card'
+import BlogCard from "./card";
 
-import { helloWorld } from "@/app/lib/db"
-
+import { helloWorld } from "@/app/lib/db";
 
 async function getData() {
-    // 1 endpoint - API?
-    const domain = getDomain()
-    const endpoint = `${domain}/api/posts` // -> third party api request??
-    // const res = await fetch(endpoint, {next: {revalidate: 10 }}) // HTTP GET
-    const res = await fetch(endpoint, {cache: 'no-store' }) // HTTP GET
+  // 1 endpoint - API?
+  const domain = getDomain();
+  const endpoint = `${domain}/api/posts`; // -> third party api request??
+  // const res = await fetch(endpoint, {next: {revalidate: 10 }}) // HTTP GET
+  const res = await fetch(endpoint, { cache: "no-store" }); // HTTP GET
 
-    if (!res.ok) {
-        throw new Error("Failed to fetch data")
-    }
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
 
-    if (res.headers.get("content-type") !== "application/json") {
-        return {items: []}
-    }
-    return res.json()
+  if (res.headers.get("content-type") !== "application/json") {
+    return { items: [] };
+  }
+  return res.json();
 }
-
 
 export default async function BlogPage() {
-    const data = await getData()
-    const dbHello = await helloWorld()
-    const items = data && data.items ? [...data.items] : []
-    return <main>
-        <h1>Hello World</h1>
-        <p>DB Response: {JSON.stringify(dbHello)}</p>
-        <p>Posts:</p>
-        {items && items.map((item, idx)=>{
-            return <BlogCard title={item.title} key={`post-${idx}`} />
+  const data = await getData();
+  const dbHello = await helloWorld();
+  const items = data && data.items ? [...data.items] : [];
+  return (
+    <main>
+      <h1>Hello World</h1>
+      <p>DB Response: {JSON.stringify(dbHello)}</p>
+      <p>Posts:</p>
+      {items &&
+        items.map((item, idx) => {
+          return <BlogCard title={item.title} key={`post-${idx}`} />;
         })}
     </main>
+  );
 }
 
-export const runtime = 'edge' // nodejs
-export const preferredRegion = "iad1"
+export const runtime = "edge"; // nodejs
+export const preferredRegion = "fra1";
