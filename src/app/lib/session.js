02ec67adda2a640fe1d6ecdfdb1be9f3dfd00056
NeelClaudel <neel.claudel@gmail.com>
2023-10-19 16:12:45 +0200

    modified:   src/app/api/links/route.js 	modified:   src/app/lib/db.js 	modified:   src/app/lib/session.js 	renamed:    src/migrations/0000_graceful_winter_soldier.sql -> src/migrations/0000_normal_hitman.sql 	modified:   src/migrations/meta/0000_snapshot.json 	modified:   src/migrations/meta/_journal.json

--- src/app/lib/session.js
+++ src/app/lib/session.js
@@ -1,58 +1,55 @@
-import * as jose from "jose"
-import {cookies} from 'next/headers'
+import * as jose from "jose";
+import { cookies } from "next/headers";
 
-// const secret = jose.base64url.decode(process.env.JOSE_SESSION_KEY)
-const secret = new TextEncoder().encode(process.env.JOSE_SESSION_KEY)
-const issuer = 'urn:jrefio:issuer'
-const audience = 'urn:jrefio:audience'
-const expiresAt = '2h'
+// TODO: place all following values in .env file
+//const secret = jose.base64url.decode(process.env.JOSE_SESSION_KEY);
+const secret = new TextEncoder().encode(process.env.JOSE_SESSION_KEY);
+const issuer = "urn:jrefio:issuer";
+const audience = "urn:jrefio:audience";
+const expiresAt = "2h";
 
+//TODO: place algorythm and encoding value in .env file as well for security
 export const encodeUserSession = async (userId) => {
-    const jwt = await new jose.EncryptJWT({ 'user': userId })
-        .setProtectedHeader({ alg: 'dir', enc: 'A128CBC-HS256' })
-        .setIssuedAt()
-        .setIssuer(issuer)
-        .setAudience(audience)
-        .setExpirationTime(expiresAt)
-        .encrypt(secret)
-    return jwt
-}
-
+  const jwt = await new jose.EncryptJWT({ user: userId })
+    .setProtectedHeader({ alg: "dir", enc: "A128CBC-HS256" })
+    .setIssuedAt()
+    .setIssuer(issuer)
+    .setAudience(audience)
+    .setExpirationTime(expiresAt)
+    .encrypt(secret);
+  return jwt;
+};
 
 export const decodeUserSession = async (jwt) => {
-    try {
-        const { payload } = await jose.jwtDecrypt(jwt, secret, {
-            issuer: issuer,
-            audience: audience,
-        })
-        const {user} = payload
-        return user
-    } catch (error) {
-        
-    }
-    return null
-
-} 
-
+  try {
+    const { payload } = await jose.jwtDecrypt(jwt, secret, {
+      issuer: issuer,
+      audience: audience,
+    });
+    const { user } = payload;
+    return user;
+  } catch (error) {}
+  return null;
+};
 
 export const setSessionUser = async (userId) => {
-    const newSessionValue = await encodeUserSession(userId)
-    // call in routes.js
-    cookies().set("session_id", newSessionValue)
-}
+  const newSessionValue = await encodeUserSession(userId);
+  // call in routes.js
+  cookies().set("session_id", newSessionValue);
+};
 
 export const getSessionUser = async () => {
-    const cookieSessionValue = cookies().get("session_id")
-    if (!cookieSessionValue) {
-        return null
-    }
-    const extractedUserId = await decodeUserSession(cookieSessionValue.value)
-    if (!extractedUserId) {
-        return null
-    }
-    return extractedUserId
-}
+  const cookieSessionValue = cookies().get("session_id");
+  if (!cookieSessionValue) {
+    return null;
+  }
+  const extractedUserId = await decodeUserSession(cookieSessionValue.value);
+  if (!extractedUserId) {
+    return null;
+  }
+  return extractedUserId;
+};
 
 export const endSessionForUser = async () => {
-    cookies().delete("session_id")
-}
+  cookies().delete("session_id");
+};
