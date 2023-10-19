0426863fb57e6cca971080364fcc90f941fbcfac
NeelClaudel <neel.claudel@gmail.com>
2023-10-19 05:44:54 +0200

    modified:   src/app/globals.css 	modified:   src/app/page.js 	modified:   src/app/ui/navbar.js

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default async function Home() {
   const user = await getSessionUser();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>shorteo.io is coming soon!</h1>
+      <h1 className="text-slate-800">shorteo.io is coming soon!</h1>
       {user && <div>{user}</div>}
     </main>
   );
--- src/app/ui/navbar.js
+++ src/app/ui/navbar.js
@@ -1,105 +1,84 @@
-'use client';
+"use client";
 
-import { Dropdown, Navbar } from 'flowbite-react';
-import Image from 'next/image';
-import Link from 'next/link';
+import { Dropdown, Navbar } from "flowbite-react";
+import Image from "next/image";
+import Link from "next/link";
 
 export function NavbarForAnon() {
-    return (
-      <Navbar
-        fluid
-        rounded
-      >
-        <Navbar.Brand href="/">
-          <Image alt="Jref.io Log"
-            className="mr-3 h-6 sm:h-9"
-            width={40}
-            height={40}
-            src="/favicon.ico" />
-          <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-            jref.io
-          </span>
-        </Navbar.Brand>
-        
-        <Navbar.Collapse>
-          <Navbar.Link
-            active
-            href="/"
-          >
-            <p>
-              Home
-            </p>
-          </Navbar.Link>
-          <Navbar.Link href="/links">
-            Links
-          </Navbar.Link>
-          <Navbar.Link href="/register">
-            Sign up
-          </Navbar.Link>
-          <Navbar.Link href="/login">
-            Login
-          </Navbar.Link>
-          
-        </Navbar.Collapse>
-      </Navbar>
-    )
-  }
-  
-
-export default function NavbarForUser() {
   return (
-    <Navbar
-      fluid
-      rounded
-    >
+    <Navbar fluid rounded>
       <Navbar.Brand href="/">
-        <Image alt="jref.io Logo"
+        <Image
+          alt="Shorteo.io Log"
           className="mr-3 h-6 sm:h-9"
           width={40}
           height={40}
-          src="/favicon.ico" />
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-          jref.io
+          src="/favicon.ico"
+        />
+        <span className="text-slate-800 self-center whitespace-nowrap text-xl font-semibold">
+          Shorteo.io
+        </span>
+      </Navbar.Brand>
+
+      <Navbar.Collapse>
+        <Navbar.Link active href="/">
+          <p>Home</p>
+        </Navbar.Link>
+        <Navbar.Link href="/links">Links</Navbar.Link>
+        <Navbar.Link href="/register">Sign up</Navbar.Link>
+        <Navbar.Link href="/login">Login</Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+
+export default function NavbarForUser() {
+  return (
+    <Navbar fluid rounded>
+      <Navbar.Brand href="/">
+        <Image
+          alt="shorteo.io Logo"
+          className="mr-3 h-6 sm:h-9"
+          width={40}
+          height={40}
+          src="/favicon.ico"
+        />
+        <span className="text-slate-800 self-center whitespace-nowrap text-xl font-semibold">
+          Shorteo.io
         </span>
       </Navbar.Brand>
       <div className="flex md:order-2">
         <Dropdown
           inline
-          label={<Image alt="jref.io Logo"
-          className="mr-3 w-3 h-3 sm:w-4 sm:h-4 rounded"
-          width={40}
-          height={40}
-          src="/favicon.ico" />}
+          label={
+            <Image
+              alt="jref.io Logo"
+              className="mr-3 w-3 h-3 sm:w-4 sm:h-4 rounded"
+              width={40}
+              height={40}
+              src="/favicon.ico"
+            />
+          }
         >
           <Dropdown.Item>
-            <Link href='/'>Dashboard</Link> 
+            <Link href="/">Dashboard</Link>
           </Dropdown.Item>
           <Dropdown.Item>
-          <Link href='/links'>Links</Link> 
+            <Link href="/links">Links</Link>
           </Dropdown.Item>
           <Dropdown.Divider />
           <Dropdown.Item>
-            <Link href='/logout'>Logout</Link> 
+            <Link href="/logout">Logout</Link>
           </Dropdown.Item>
         </Dropdown>
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
-        <Navbar.Link
-          active
-          href="/"
-        >
-          <p>
-            Home
-          </p>
+        <Navbar.Link active href="/">
+          <p>Home</p>
         </Navbar.Link>
-        <Navbar.Link href="/links">
-          Links
-        </Navbar.Link>
-        
+        <Navbar.Link href="/links">Links</Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
-  )
+  );
 }
-
-
